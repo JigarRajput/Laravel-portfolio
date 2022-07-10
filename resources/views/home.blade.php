485ceb3b524d546d5561c1dbc6801c47c4ba110c
JigarRajput <rajputjigar455@gmail.com>
2022-07-11 00:00:34 +0530

    Made responsive home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
             </div>
 
             <div class="col-sm-6 d-flex align-items-center">
-                <img src="https://scontent.fpnq14-1.fna.fbcdn.net/v/t1.6435-9/103335487_260138518591581_8081209284784662624_n.jpg?stp=dst-jpg_p843x403&_nc_cat=108&ccb=1-7&_nc_sid=e3f864&_nc_ohc=XRLy2Dn6R_MAX-lj2Un&_nc_ht=scontent.fpnq14-1.fna&oh=00_AT-BVR0uomV_KwoOCGqnePX7XS3T5J95Bp_3ZeP7dhn5IQ&oe=62F03878" class="mx-sm-auto ms-5" style="width:80%; height: 76%; border-radius: 50%; border: 8px solid black ">
+                <img src="https://scontent.fpnq14-1.fna.fbcdn.net/v/t1.6435-9/103335487_260138518591581_8081209284784662624_n.jpg?stp=dst-jpg_p843x403&_nc_cat=108&ccb=1-7&_nc_sid=e3f864&_nc_ohc=XRLy2Dn6R_MAX-lj2Un&_nc_ht=scontent.fpnq14-1.fna&oh=00_AT-BVR0uomV_KwoOCGqnePX7XS3T5J95Bp_3ZeP7dhn5IQ&oe=62F03878" class="ms-sm-auto ms-5" style="width:80%; height: 76%; border-radius: 50%; border: 8px solid black ">
             </div>
         <!-- <div class="row" style="">
             <div class="col-sm-6">
