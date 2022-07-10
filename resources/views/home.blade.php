8b566bde922f27558f4071d21865e9d0901e91b0
JigarRajput <rajputjigar455@gmail.com>
2022-07-10 23:59:08 +0530

    made responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
 
             <div class="col-sm-6 d-flex flex-row align-items-center">
                 <div class="d-flex flex-column ms-5">
-                    <h3>Hello There!!</h3><br/>
+                    <h3 class ="mt-4">Hello There!!</h3><br/>
                     <i style="font-family: Verdana"><h1> Jigar Rajput____</h1></i> 
                     <h4 class="mt-5">Web developer</h4>
                     <h6 class="mt-2">Reactjs, Laravel, HTML5, CSS3, BOOTSTRAP 5, Spring Boot, POSTGRESQL, MYSQL, Firebase</h6>
@@ -14,7 +14,7 @@
             </div>
 
             <div class="col-sm-6 d-flex align-items-center">
-                <img src="https://scontent.fpnq14-1.fna.fbcdn.net/v/t1.6435-9/103335487_260138518591581_8081209284784662624_n.jpg?stp=dst-jpg_p843x403&_nc_cat=108&ccb=1-7&_nc_sid=e3f864&_nc_ohc=XRLy2Dn6R_MAX-lj2Un&_nc_ht=scontent.fpnq14-1.fna&oh=00_AT-BVR0uomV_KwoOCGqnePX7XS3T5J95Bp_3ZeP7dhn5IQ&oe=62F03878" class="ms-auto" style="width:80%; height: 76%; border-radius: 50%; border: 8px solid black ">
+                <img src="https://scontent.fpnq14-1.fna.fbcdn.net/v/t1.6435-9/103335487_260138518591581_8081209284784662624_n.jpg?stp=dst-jpg_p843x403&_nc_cat=108&ccb=1-7&_nc_sid=e3f864&_nc_ohc=XRLy2Dn6R_MAX-lj2Un&_nc_ht=scontent.fpnq14-1.fna&oh=00_AT-BVR0uomV_KwoOCGqnePX7XS3T5J95Bp_3ZeP7dhn5IQ&oe=62F03878" class="mx-sm-auto ms-5" style="width:80%; height: 76%; border-radius: 50%; border: 8px solid black ">
             </div>
         <!-- <div class="row" style="">
             <div class="col-sm-6">
--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
 <div class="row my-5">
 
-    <div class="card ms-4" style="width:350px">
+    <div class="card ms-sm-4 ms-4 mb-sm-4 mb-4" style="width:350px">
         <div class="card-body">
         <div class="card-title"><strong>Book-Baazar</strong></div>
             Full stack web application using Reactjs, PostgreSQL, and SpringBoot Rest API
@@ -12,7 +12,7 @@
         <a href="http://book-baazar.herokuapp.com/" class="btn btn-dark m-4">Visit project</a>
     </div>
 
-    <div class="card ms-2" style="width:350px">
+    <div class="card ms-sm-2 ms-4 mb-sm-4 mb-4" style="width:350px">
         <div class="card-body">
         <div class="card-title"><strong>Rest API</strong></div>
             A complete API developed using Spring Boot and POSTGRESQL
@@ -20,7 +20,7 @@
         <a href="https://github.com/JigarRajput/spring-boot-postgres-restapi" class="btn btn-dark m-4">Visit project</a>
     </div>
 
-    <div class="card ms-2" style="width:350px">
+    <div class="card ms-sm-2 ms-4 mb-sm-4 mb-4" style="width:350px">
         <div class="card-body">
         <div class="card-title"><strong>Basic Banking System</strong></div>
                 Basic Web application developed using Bootstrap 5, JSP, and MYSQL
@@ -33,7 +33,7 @@
 
 <div class="row">
 
-    <div class="card ms-4" style="width:350px">
+    <div class="card ms-sm-4 ms-4 mb-sm-4 mb-4" style="width:350px">
         <div class="card-body">
         <div class="card-title"><strong>NewsApp- Android App</strong></div>
            Simple news app to read news for different news agencies !!
@@ -41,7 +41,7 @@
         <a href="https://github.com/JigarRajput/NewsApplication-Android" class="btn btn-dark m-4">Visit project</a>
     </div>
 
-    <div class="card ms-2" style="width:350px">
+    <div class="card ms-sm-2 ms-4 mb-sm-4 mb-4" style="width:350px">
         <div class="card-body">
         <div class="card-title"><strong>Shop manager- Android App</strong></div>
             Android app to maintain the list of shopping items
@@ -49,7 +49,7 @@
         <a href="http://book-baazar.herokuapp.com/" class="btn btn-dark m-4">Visit project</a>
     </div>
 
-    <div class="card ms-2" style="width:350px">
+    <div class="card ms-sm-2 ms-4 mb-sm-4 mb-4" style="width:350px">
         <div class="card-body">
         <div class="card-title"><strong>Portfolio - Laravel</strong></div>
             portfolio website using Laravel blade templates
